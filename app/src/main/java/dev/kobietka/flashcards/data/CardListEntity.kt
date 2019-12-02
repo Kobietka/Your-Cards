6fc17f6d1bbe088ad6f334bd46948f8eda07c03b
Kobietka <slawekgrawgry@gmail.com>
2019-12-02 22:12:18 +0100

    Add CardListEntity

---
+++ app/src/main/java/dev/kobietka/flashcards/data/CardListEntity.kt
@@ -0,0 +1,17 @@
+package dev.kobietka.flashcards.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import dev.kobietka.flashcards.domain.models.Flashcard
+
+@Entity(tableName = "cardList")
+data class CardListEntity(@PrimaryKey val id: Int?,
+                          @ColumnInfo val name: String,
+                          @ColumnInfo val count: Int,
+                          @ColumnInfo val endless: Boolean,
+                          @ColumnInfo val typingAnswer: Boolean,
+                          @ColumnInfo val randomOrder: Boolean,
+                          @ColumnInfo val flashcards: List<Flashcard>)
+
+
