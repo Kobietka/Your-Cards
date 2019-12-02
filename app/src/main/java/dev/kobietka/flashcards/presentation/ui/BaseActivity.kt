3d31765b6b3b4ecda0cf3551f787d6aef4e91cab
Kobietka <slawekgrawgry@gmail.com>
2019-12-02 21:49:24 +0100

    Initial commit

---
+++ app/src/main/java/dev/kobietka/flashcards/presentation/ui/BaseActivity.kt
@@ -0,0 +1,8 @@
+package dev.kobietka.flashcards.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+
+}
---
+++ app/src/main/java/dev/kobietka/flashcards/presentation/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.kobietka.flashcards
+
+import android.os.Bundle
+import dev.kobietka.flashcards.presentation.ui.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
