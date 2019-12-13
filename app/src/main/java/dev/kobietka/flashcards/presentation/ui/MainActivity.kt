d86d651922e241a260564f4624fe434f2dea6ae6
Kobietka <slawekgrawgry@gmail.com>
2019-12-13 21:20:45 +0100

    Move recyclerView from mainActivity to mainFragment

--- app/src/main/java/dev/kobietka/flashcards/presentation/ui/MainActivity.kt
+++ app/src/main/java/dev/kobietka/flashcards/presentation/ui/MainActivity.kt
@@ -1,26 +1,13 @@
 package dev.kobietka.flashcards
 
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import dev.kobietka.flashcards.presentation.ui.common.BaseActivity
-import dev.kobietka.flashcards.presentation.ui.rvs.MainAdapter
-import javax.inject.Inject
+
 
 class MainActivity : BaseActivity() {
-
-    //lateinit var recyclerView: RecyclerView
-    //@Inject lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        /*presentationComponent.inject(this)
-        recyclerView = findViewById(R.id.rv_main)
-        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerView.adapter = adapter*/
     }
-
 }
--- app/src/main/java/dev/kobietka/flashcards/presentation/ui/fragmentmain/MainFragment.kt
+++ app/src/main/java/dev/kobietka/flashcards/presentation/ui/fragmentmain/MainFragment.kt
@@ -2,13 +2,25 @@ package dev.kobietka.flashcards.presentation.ui.fragmentmain
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.kobietka.flashcards.R
 import dev.kobietka.flashcards.presentation.ui.common.BaseFragment
+import dev.kobietka.flashcards.presentation.ui.rvs.MainAdapter
+import javax.inject.Inject
 
 class MainFragment: BaseFragment() {
 
+    lateinit var recyclerView: RecyclerView
+    @Inject lateinit var adapter: MainAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        presentationComponent.inject(this)
+
+        recyclerView = view.findViewById(R.id.rv_main)
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = adapter
     }
 
     override fun getLayout(): Int {
