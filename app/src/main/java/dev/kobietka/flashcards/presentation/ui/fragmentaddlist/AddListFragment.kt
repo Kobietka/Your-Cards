4b531485fd169d8eb12033212a09f2c684b32cff
Kobietka <slawekgrawgry@gmail.com>
2019-12-15 06:48:00 +0100

    Set up the recycler view in fragment add list

--- app/src/main/java/dev/kobietka/flashcards/presentation/ui/fragmentaddlist/AddListFragment.kt
+++ app/src/main/java/dev/kobietka/flashcards/presentation/ui/fragmentaddlist/AddListFragment.kt
@@ -1,8 +1,11 @@
 package dev.kobietka.flashcards.presentation.ui.fragmentaddlist
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.kobietka.flashcards.R
 import dev.kobietka.flashcards.presentation.ui.common.BaseFragment
@@ -13,17 +16,20 @@ import javax.inject.Inject
 class AddListFragment : BaseFragment() {
 
     lateinit var cancelButton: ImageButton
+    lateinit var createList: Button
     lateinit var recyclerView: RecyclerView
     @Inject lateinit var adapter: FlashcardAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        recyclerView
-
+        recyclerView = view.findViewById(R.id.recycler_add_list)
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = adapter
 
         cancelButton = view.findViewById(R.id.button_cancel_add_list)
 
+        
         cancelButton.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.main_container, MainFragment())
